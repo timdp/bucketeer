f3ae82d077ecfaea10bd30da88ee330198f4070b
Tim De Pauw <tim.depauw@zentrick.com>
2014-10-02 12:06:50 +0200

    Optional format for logging

--- filters/log.js
+++ filters/log.js
@@ -2,6 +2,6 @@ var debug = require('debug')('bucketeer/filter/log');
 
 module.exports = function(obj, options, cb) {
   debug('run', obj.Key);
-  console.log(obj.Key);
+  console.log(typeof options.format === 'string' ? options.format : '%s', obj.Key);
   cb(null, true);
 };
